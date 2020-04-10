3878ce21bec59763e4dfae840b211ca9d9ee1d52
akilandeshwariakilmurali <62900349+akilandeshwariakilmurali@users.noreply.github.com>
2020-04-10 18:06:51 +0530

    AUI Learning Mouse hover

--- MouseHover.java
+++ MouseHover.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class MouseHover {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// set the system property
 		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
 
@@ -20,27 +20,30 @@ public class MouseHover {
 
 		// Implicit wait for 10 seconds
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
 		// Close X
 		driver.findElementByXPath("//button[text()='✕']").click();
-		
+
 		// Maximize
 		driver.manage().window().maximize();
-		
+
 		// Locate Electronics
 		WebElement electronics = driver.findElementByXPath("//span[text()='Electronics']");
-		
-		//Mouse Hover electronics
+
+		// Mouse Hover electronics
 		Actions act = new Actions(driver);
 		act.moveToElement(electronics).perform();
-		
+
 		// CLick MI
 		driver.findElementByLinkText("Mi").click();
+
+		Thread.sleep(3000);
 		
 		// Verify title
 		if (driver.getTitle().contains("Mi Mobile")) {
-			System.out.println(" In correct page");
+			System.out.println(" In MI page");
 		}
+
 	}
 
 }

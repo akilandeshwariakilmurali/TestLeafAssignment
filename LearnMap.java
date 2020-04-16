8e8477001344493048e5c0c6b1f39c19e00634cb
akilandeshwariakilmurali <62900349+akilandeshwariakilmurali@users.noreply.github.com>
2020-04-16 23:36:51 +0530

    Week3 Day4

---
+++ LearnMap.java
@@ -0,0 +1,43 @@
+package week3.day4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LearnMap {
+
+	public static void main(String[] args) {
+		
+		
+		// Get all the Train Names  
+		Map<String,String> mp = new HashMap<String,String>();
+		
+		mp.put("A001", "Test1");
+		mp.put("A002", "Test2");
+		mp.put("A003", "Test3");
+		
+		// Printing all available items
+		for(Entry<String,String> eachItem : mp.entrySet()) {
+			System.out.println(eachItem);
+			System.out.println(eachItem.getKey());
+			System.out.println(eachItem.getValue());
+		}
+		
+		// To remove specific item
+		mp.remove("A003");
+		
+		// Checking whether it is empty
+		System.out.println(mp.isEmpty());
+		
+		// Count
+		System.out.println(mp.size());
+		
+		// Contains specific key
+		boolean pre = mp.containsKey("A003");
+		System.out.println(pre);
+		
+		boolean pre1 = mp.containsKey("A002");
+		System.out.println(pre1);
+	}
+
+}
